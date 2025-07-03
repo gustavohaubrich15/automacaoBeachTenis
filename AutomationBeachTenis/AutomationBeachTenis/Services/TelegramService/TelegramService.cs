af360d8ebe965ae0ab0669a4b6c6c3e5bb6102a2
Gustavo Haubrich <gustavohaubrich15@gmail.com>
2025-07-03 20:50:28 -0300

    Update TelegramService.cs

--- AutomationBeachTenis/AutomationBeachTenis/Services/TelegramService/TelegramService.cs
+++ AutomationBeachTenis/AutomationBeachTenis/Services/TelegramService/TelegramService.cs
@@ -1,18 +1,17 @@
-﻿using System.Text;
+using System.Text;
 using AutomationBeachTenis.Response;
 using AutomationBeachTenis.Services.GenericApiService;
 using AutomationBeachTenis.Services.TournamentService;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 
 namespace AutomationBeachTenis.Services.TelegramService
 {
     public class TelegramService : ITelegramService
     {
         private readonly ILogger<TelegramService> _logger;
-        private readonly IGenericApiService _genericApiService;
         private readonly IConfiguration _configuration;
         public TelegramBotClient _telegramBotClient;
-        private string UrlTelegram => $"{_configuration["Telegram:SendMessageApi"]}{_configuration["Telegram:BotToken"]}/sendMessage";
 
         public TelegramService(ILogger<TelegramService> logger,
             IGenericApiService genericApiService,
@@ -21,26 +20,18 @@ namespace AutomationBeachTenis.Services.TelegramService
         {
 
             _logger = logger;
-            _genericApiService = genericApiService;
             _configuration = configuration;
             _telegramBotClient = new TelegramBotClient(_configuration["Telegram:BotToken"]);
         }
 
         public async Task SendMessageTelegramToChannel(StringBuilder message)
         {
-            var fullMessage = message.ToString();
-            const int maxLength = 4000;
-
-            for (int i = 0; i < fullMessage.Length; i += maxLength)
-            {
-                var chunk = fullMessage.Substring(i, Math.Min(maxLength, fullMessage.Length - i));
-                await SendMessage(chunk);
-            }
+            await _telegramBotClient.SendMessage(_configuration["Telegram:ChatId"], message.ToString(), parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
         }
 
-        private async Task SendMessage(string chunk)
+        public async Task SendPhotoTelegramToChannel(Stream photoStream, string typePlay)
         {
-            await _telegramBotClient.SendMessage(_configuration["Telegram:ChatId"], chunk);
+            await _telegramBotClient.SendPhoto(_configuration["Telegram:ChatId"], photoStream, typePlay, ParseMode.Html);
         }
 
     }
